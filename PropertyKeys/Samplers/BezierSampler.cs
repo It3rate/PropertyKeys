9cd19c79b8f169c5afcba5f2791fb6f17aca8441
Robin Debreuil <it3rate@gmail.com>
2019-09-23 12:10:42 -0500

    Fixed up BezierSampler to not use internal Series store.

--- PropertyKeys/Samplers/BezierSampler.cs
+++ PropertyKeys/Samplers/BezierSampler.cs
@@ -4,53 +4,56 @@ namespace DataArcs.Samplers
 {
 	public class BezierSampler : Sampler
 	{
-		public BezierSeries Series;
-
-		public BezierSampler()
-		{
-		}
-
-		public BezierSampler(BezierSeries series)
-		{
-			Series = series;
-			Capacity = series.Count;
-		}
 
 		public override Series GetValueAtIndex(Series series, int index)
 		{
-			// todo: check if this virtualCount assignment should happen in beziers at this point or pass through.
-			series = series ?? Series;
-			var t = index / (float) Capacity;
+			// not Bezier series by overloads
+            float capacity = (series as BezierSeries)?.Moves.Length ?? series.Count;
+			var t = index / capacity;
 			return GetValueAtT(series, t);
 		}
 
-		public override Series GetValueAtT(Series series, float t)
+        public override Series GetValueAtT(Series series, float t)
+        {
+	        // not Bezier series by overloads
+            SeriesUtils.GetScaledT(t, series.Count, out var vT, out var startIndex, out var endIndex);
+	        var aSeries = series.GetDataAtIndex(startIndex);
+	        var a = aSeries.GetValueAtVirtualIndex(startIndex == endIndex ? 0 : aSeries.Count - 1, aSeries.Count).FloatData;
+	        var b = series.GetDataAtIndex(endIndex).FloatData; // GetFloatArrayAtIndex(endIndex);
+	        var moveType = b.Length > 2 ? BezierMove.CubeTo : BezierMove.LineTo;// startIndex < series.Moves.Length ? series.Moves[startIndex] : BezierMove.LineTo;
+	        return GetSeriesAtT(a, b, moveType, vT);
+        }
+
+		public Series GetValueAtIndex(BezierSeries series, int index)
 		{
-			series = series ?? Series;
-			var moves = series is BezierSeries bezierSeries ? bezierSeries.Moves : new[] {BezierMove.LineTo};
-			return GetSeriesAtT(series, t);
+			var t = index / Capacity;
+			return GetValueAtT(series, t);
 		}
-		
-		private Series GetSeriesAtT(Series series, float t)
+        public Series GetValueAtT(BezierSeries series, float t)
+        {
+	        SeriesUtils.GetScaledT(t, Capacity, out var vT, out var startIndex, out var endIndex);
+	        var aSeries = series.GetDataAtIndex(startIndex);
+		    var a = aSeries.GetValueAtVirtualIndex(startIndex == endIndex ? 0 : aSeries.Count - 1, aSeries.Count).FloatData;
+	        var b = series.GetDataAtIndex(endIndex).FloatData; // GetFloatArrayAtIndex(endIndex);
+	        var moveType = startIndex < series.Moves.Length ? series.Moves[startIndex] : BezierMove.LineTo;
+	        return GetSeriesAtT(a, b, moveType, vT);
+        }
+
+        private Series GetSeriesAtT(float[] a, float[] b, BezierMove moveType, float t)
 		{
-			SeriesUtils.GetScaledT(t, Series.Moves.Length, out var vT, out var startIndex, out var endIndex);
-			var a = series.GetDataAtIndex(startIndex).FloatData; // GetFloatArrayAtIndex(startIndex);
-			var b = series.GetDataAtIndex(endIndex).FloatData; // GetFloatArrayAtIndex(endIndex);
-			var p0Index = startIndex == endIndex ? 0 : a.Length - 2; // start from last point unless at start or end.
 			var p2Index = b.Length - 2;
-			var moveType = startIndex < Series.Moves.Length ? Series.Moves[startIndex] : BezierMove.LineTo;
 			float[] result = {0, 0};
 			var it = 1f - t;
 			switch (moveType)
 			{
 				case BezierMove.MoveTo:
 				case BezierMove.LineTo:
-					result[0] = a[p0Index] + (b[p2Index] - a[p0Index]) * t;
-					result[1] = a[p0Index + 1] + (b[p2Index + 1] - a[p0Index + 1]) * t;
+					result[0] = a[0] + (b[p2Index] - a[0]) * t;
+					result[1] = a[1] + (b[p2Index + 1] - a[1]) * t;
 					break;
 				case BezierMove.QuadTo:
-					result[0] = it * it * a[p0Index] + 2 * it * t * b[0] + t * t * b[p2Index];
-					result[1] = it * it * a[p0Index + 1] + 2 * it * t * b[1] + t * t * b[p2Index + 1];
+					result[0] = it * it * a[0] + 2 * it * t * b[0] + t * t * b[p2Index];
+					result[1] = it * it * a[1] + 2 * it * t * b[1] + t * t * b[p2Index + 1];
 					break;
 				case BezierMove.CubeTo:
 					// todo: cubic bezier calc
