2e241597ea258eea585b6ca5dd16ccd0da6fbfa2
Robin Debreuil <it3rate@gmail.com>
2019-11-12 12:22:14 -0600

    Added a separate reusable/threadable class for query results.

---
+++ PropertyKeys/Components/Interfaces/IQueryResult.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataArcs.Graphic;
+using DataArcs.SeriesData;
+
+namespace DataArcs.Components.Interfaces
+{
+    public interface IQueryResult
+    {
+	    float OrderT { get; }
+	    ParametricSeries IndexT { get; }
+        //float DeltaT { get; }
+        //float CurrentTime { get; }
+        //IComposite CompositeRef { get; }
+        IRenderable Renderer { get; }
+
+        Dictionary<PropertyId, Series> Properties { get; }
+
+        List<PropertyId> Keys { get; }
+        void ClearAll();
+        void AddProperty(PropertyId propertyId, Series value);
+        void Remove(PropertyId propertyId);
+
+        void Parse(IComposite composite);
+    }
+}
