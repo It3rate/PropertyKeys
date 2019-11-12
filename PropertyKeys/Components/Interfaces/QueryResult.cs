1ebab28b88c5a118466bcc54cbd2aa4075e94fa5
Robin Debreuil <it3rate@gmail.com>
2019-11-12 12:34:34 -0600

    Adjusted Query Result class.

---
+++ PropertyKeys/Components/Interfaces/QueryResult.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataArcs.Graphic;
+using DataArcs.SeriesData;
+
+namespace DataArcs.Components.Interfaces
+{
+    public class QueryResult
+    {
+        //ParametricSeries T { get; }
+        //ParametricSeries IndexT { get; }
+        public float T { get; }
+        public float IndexT { get; }
+
+        //float DeltaT { get; }
+        //float CurrentTime { get; }
+        //IComposite CompositeRef { get; }
+        public IRenderable Renderer { get; }
+
+        private Dictionary<PropertyId, Series> Properties { get; }
+
+        private List<PropertyId> Keys => Properties.Keys.ToList();
+        public void ClearAll() => Properties.Clear();
+
+        public void AddProperty(PropertyId propertyId, Series value) => Properties[propertyId] = value;
+
+        public void Remove(PropertyId propertyId) => Properties.Remove(propertyId);
+
+        public void QueryComposite(IComposite composite)
+        {
+        }
+    }
+}
